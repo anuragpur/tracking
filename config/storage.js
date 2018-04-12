d67302daab721bf35e73d48f66c1f6142705b33d
anurag <anurgpurwar@gmail.com>
2018-04-12 15:26:57 +0530

    s

--- config/storage.js
+++ config/storage.js
@@ -2,7 +2,7 @@ var storage = {};
 storage.secret= 'appsecret';
 var mongoose = require('mongoose');
 //provide a sensible default for local development
-mongodb_connection_string = 'mongodb://tracking:123456789@ds241699.mlab.com:41699/tracking';
+mongodb_connection_string = 'mongodb://127.0.0.1:27017tracking';
 //take advantage of openshift env vars when available:
 // if(process.env.OPENSHIFT_MONGODB_DB_URL){
 //     mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + 'tracking';
