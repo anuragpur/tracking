54519fbd58ff333a49518bd9c8daa57120ca5b4f
anurag <anurgpurwar@gmail.com>
2018-04-12 18:18:27 +0530

    s

--- config/storage.js
+++ config/storage.js
@@ -2,12 +2,12 @@ var storage = {};
 storage.secret= 'appsecret';
 var mongoose = require('mongoose');
 //provide a sensible default for local development
-mongodb_connection_string = 'mongodb://127.0.0.1:27017tracking';
+var url = 'mongodb://127.0.0.1:27017/tracking';
 //take advantage of openshift env vars when available:
 // if(process.env.OPENSHIFT_MONGODB_DB_URL){
 //     mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + 'tracking';
 // }
-mongoose.connect(mongodb_connection_string);
+mongoose.connect(url);
 
 var MongoClient = require('mongodb').MongoClient;
 
@@ -17,7 +17,7 @@ storage.getMongooseDB = function() {
 
 storage.getDB = function() {
     return new Promise(function(resolve, reject) {
-        MongoClient.connect(mongodb_connection_string, function(err, connection) {
+        MongoClient.connect(url, function(err, connection) {
             if (!err) {
                 console.log('db connected')
                 resolve(connection);
